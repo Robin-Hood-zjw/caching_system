a6cec5f2a2292737ecfdf0f85a49ac06464fa1cf
Robin-Hood-zjw <jiawenz@umich.edu>
2026-02-02 22:46:15 -0800

    0.3.2-creating_LRU-Node_class

--- cache/LRUCache.h
+++ cache/LRUCache.h
@@ -1,1 +1,14 @@
 #pragma once
+using namespace std;
+
+#include "CacheNode.h"
+
+template<typename Key, typename Value>
+class LRU_Node : public CacheNode<Key, Value> {
+    public:
+        weaker_ptr<LRU_Node<Key, Value>> prev;
+        shared_ptr<LRU_Node<Key, Value>> next;
+
+        LRU_Node<Key key, Value val> : CacheNode<Key, Value>(key, val) {}
+};
+
