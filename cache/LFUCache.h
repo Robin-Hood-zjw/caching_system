1a709b6dd3681f23925b2a9f6a277f1d602c82f3
Robin-Hood-zjw <jiawenz@umich.edu>
2026-02-10 15:34:05 -0800

    0.3.21-initializing_LFU_Cache

--- cache/LFUCache.h
+++ cache/LFUCache.h
@@ -1,5 +1,22 @@
 #pragma once
 
+#include "CacheList.h"
 #include "CachePolicy.h"
 
-// template<typename Key, typename Value>
+#include <mutex>
+#include <unordered_map>
+
+template<typename Key, typename Value>
+class LFU_Cache : public CachePolicy<key, Value> {
+    public:
+        using Node = typename FrequenceList<Key, Value>::Node;
+        using node_ptr = shared_ptr<Node>;
+        using node_map = unordered_map<Key, node_ptr>;
+
+    private:
+        int _capacity;
+        int minFreq;
+        mutex _mutex; 
+        node_map nodeRecords;
+        unordered_map<int, FrequenceList<Key, Value>*> freqMap;
+};
