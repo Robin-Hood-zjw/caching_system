bd69b86d23c87902c98830ea1e5f35d1bdfd4225
Robin-Hood-zjw <jiawenz@umich.edu>
2026-02-10 16:01:26 -0800

    0.3.23-crearing_LFU_public_methods

--- cache/LFUCache.h
+++ cache/LFUCache.h
@@ -16,11 +16,66 @@ class LFU_Cache : public CachePolicy<key, Value> {
         LFU_Cache(int capacity): _capacity(capacity), minFreq(INT8_MAX) {}
         ~LFU_Cache() override = default;
 
-        
+        Value get(Key key) override {
+            Value value;
+            get(key, value);
+            return value;
+        }
+
+        bool get(Key key, Value& val) override {
+            lock_guard<mutex> lock(_mutex);
+
+            if (nodeRecords.count(key)) {
+                
+                return true;
+            }
+            return false;
+        }
+
+        void put(Key key, Value& val) override {
+            if (_capacity == 0) return;
+            lock_guard<mutex> lock(_mutex);
+
+            if (nodeRecords.count(key)) {
+                nodeRecords[key]->value = val;
+                getInternal(nodeRecords[key], val);
+                return;
+            }
+
+            putInternal(key, value);
+        }
+
+        void purge() {
+            nodeRecords.clear();
+            freqMap.clear();
+        }
     private:
         int _capacity;
         int minFreq;
         mutex _mutex; 
         node_map nodeRecords;
         unordered_map<int, FrequenceList<Key, Value>*> freqMap;
+
+        // void getInternal(node_ptr node, Value& value) {
+        //     value = node->value;
+        //     removeFromList(node);
+
+        //     node->freq++;
+        //     addIntoList(node);
+        // }
+
+        // void removeFromList(node_ptr node) {
+        //     if (!node) return;
+
+        //     int freq = node->freq;
+        //     freqMap[freq]->removeNode(node);
+        // }
+
+        // void addIntoList(node_ptr node) {
+        //     if (!node) return;
+
+        //     int freq = node->freq;
+        //     if (!freqMap.count(freq)) freqMap[freq] = new FrequenceList<key, Value>(freq);
+        //     freqMap[freq]->addNode(node);
+        // }
 };
