f7ba3aee003d228a9f4e36b9335866c701bfe4ff
Robin-Hood-zjw <jiawenz@umich.edu>
2026-02-12 15:53:48 -0800

    0.4.4-initializing_ARC-LFU_backbone

---
+++ src/ARC/ArcLFU.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "ArcNode.h"
+
+#include <map>
+#include <mutex>
+#include <unordered_map>
+
+template<typename Key, typename Value>
+class ARC_LFU {
+    public:
+        using node_type = ArcNode<Key, Value>;
+        using node_ptr = std::shared_ptr<node_ptr>;
+        using node_map = std::unordered_map<Key, node_ptr>;
+        using freq_map = std::map<size_t, std::list<node_ptr>>;
+
+        explicit ARC_LFU(size_t capacity, size_t threshold):
+            _minFreq(0), _capacity(capacity), _ghostCapacity(capacity), _transformThreshold(threshold) {
+                initializeLists();
+        }
+
+        bool get(Key key, Value& value) {
+            std::lock_guard<std::mutex> lock(_mutex);
+
+            auto itr = _mainCache.find(key);
+            if (itr != _mainCache.end()) {
+                updateNodeFreq(it->second);
+                value = it->second->getValue();
+                return true;
+            }
+            return false;
+        }
+
+        bool put(Key key, Value value) {
+            if (_capacity == 0) return false;
+            std::lock_guard<std::mutex> lock(_mutex);
+
+            auto it = _mainCache.find(key);
+
+            return it != _mainCache.end() ? 
+                updateExistingNode(it->second, value) : addNewNode(key, value);
+        }
+    private:
+        size_t _minFreq;
+        size_t _capacity;
+        size_t _ghostCapacity;
+        size_t _transformThreshold;
+
+        std::mutex _mutex;
+
+        node_map _mainCache;
+        node_map _ghostCache;
+        freq_map _freqMap;
+
+        node_ptr _ghostHead;
+        node_ptr _ghostTail;
+
+        void initializeLists() {
+            _ghostHead = std::make_shared<node_type>();
+            _ghostTail = std::make_shared<node_type>();
+            _ghostHead->next = _ghostTail;
+            _ghostTail->prev = _ghostHead;
+        }
+
+
+};
