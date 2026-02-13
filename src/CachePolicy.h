226b441a2481ba82cfdbddc24a9add0c0f26f02c
Robin-Hood-zjw <jiawenz@umich.edu>
2026-02-12 17:32:18 -0800

    0.4.7-creating_ARC-Cache_backbone

--- src/CachePolicy.h
+++ src/CachePolicy.h
@@ -6,6 +6,6 @@ class CachePolicy {
         virtual ~CachePolicy() {};
 
         virtual Value get(Key key) = 0;
-        virtual bool get(Key key, Value& val) = 0;
-        virtual void put(Key key, Value val) = 0;
+        virtual bool get(Key key, Value& value) = 0;
+        virtual void put(Key key, Value value) = 0;
 };
