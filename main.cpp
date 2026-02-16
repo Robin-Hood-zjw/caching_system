3032c4ec23e97b5f7fa76eb675ed705b53c062ce
Robin-Hood-zjw <jiawenz@umich.edu>
2026-02-16 12:40:40 -0800

    0.7.3-building_Timer_class

--- main.cpp
+++ main.cpp
@@ -11,6 +11,19 @@
 #include <iostream>
 #include <algorithm>
 
+class Timer {
+    public:
+        Timer(): _start(std::chrono::high_resolution_clock::now()) {}
+
+        double elapsed() {
+            auto now = std::chrono::high_resolution_clock::now();
+            return std::chrono::duration_cast<std::chrono::milliseconds>(now - _start).count();
+        }
+
+    private:
+        std::chrono::time_point<std::chrono::high_resolution_clock> _start;
+};
+
 int main() {
     return 0;
-}
+};
